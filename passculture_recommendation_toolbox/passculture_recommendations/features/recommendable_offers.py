ec427f2a6e3521297a3c60fe63983972dae04138
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2020-07-31 12:27:40 +0200

    refactoring and updating requirements

--- passculture_recommendation_toolbox/passculture_recommendations/features/recommendable_offers.py
+++ passculture_recommendation_toolbox/passculture_recommendations/features/recommendable_offers.py
@@ -1,7 +1,5 @@
-from passculture_recommendations.data.feature_engineering import get_a_df_from_sql_query
-from sqlalchemy import create_engine
-engine = create_engine('postgres://pass_culture:passq@localhost:5434/pass_culture?sslmode=prefer')
-connection = engine.connect()
+from passculture_recommendations.features.feature_engineering import get_a_df_from_sql_query
+
 
 def get_all_the_recommendable_offers():
     query = """SELECT * FROM discovery_view"""
@@ -59,7 +57,7 @@ def get_all_the_recommendable_offers():
 
     return recommendable_offers_to_all_the_users
 
-def get_all_the_recommendable_offers_from_bretagne(recommendable_offers_to_all_the_users):
+def get_all_the_recommendable_offers_from_bretagne(connection, recommendable_offers_to_all_the_users):
     bretagne = ['22', '29', '35', '56']
     recommendable_offers_to_all_the_users_in_bretagne = recommendable_offers_to_all_the_users[recommendable_offers_to_all_the_users['offer_pc'].isin(bretagne)]
 
