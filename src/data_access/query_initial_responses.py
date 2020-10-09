47776361ed1928bcb1f89ea027cad304b2fb851c
Jean <jeanr@sicara.com>
2020-10-09 18:20:54 +0200

    (PC-4605): review corrections

--- src/data_access/query_initial_responses.py
+++ src/data_access/query_initial_responses.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 import tqdm
@@ -18,53 +19,59 @@ class QueryInitialResponses:
             self.form_dict = form_dict
         else:
             self.form_dict = {
-                "prod": "Oqu7Ag",
-                "testing": "T8rurj",
-                "staging": "OoEtVb"
+                "prod": os.getenv("PROD_FORM_ID"),
+                "testing": os.getenv("TESTING_FORM_ID"),
+                "staging": os.getenv("STAGING_FORM_ID")
             }
         self.api_key = api_key
 
-    def query_page(self, nb_items_per_page, page_index):
+    def query_page(self, nb_items_per_page=1000, until=None):
         """
         Typeform responses are stored in pages of {nb_items_per_page} items each (except possibly the last one).
-        Query the page of index {page_index} of these typeform responses.
+        Query a page of responses, possibly submitted until a specified time.
         Args:
             nb_items_per_page (int): number of items in each page
-            page_index (int): index of the page queried
+            until (string): limit request to responses submitted until the specified date and time.
+                In ISO 8601 format, UTC time, to the second, with T as a delimiter between the date and time.
 
         Returns:
             requests.models.Response: a requests response.
         """
-        res = requests.get(
-            f"https://api.typeform.com/forms/{self.form_dict[self.env]}/responses?page_size={nb_items_per_page}&page={page_index}",
+        request_url = f"https://api.typeform.com/forms/{self.form_dict[self.env]}/responses?page_size={nb_items_per_page}"
+        if until is not None:
+            request_url += f"&until={until}"
+
+        result = requests.get(
+            request_url,
             headers={"Authorization": f"Bearer {self.api_key}"}
         )
-        if res.status_code != 200:
+        if result.status_code != 200:
             raise ApiQueryError(
-                f"Error {res.status_code} when querying typeform API on {self.form_dict[self.env]} form.")
-        return res
+                f"Error {result.status_code} when querying typeform API on {self.form_dict[self.env]} form.")
+        return result
 
-    def query_all(self, nb_items_per_page=30, last_page=None):
+    def query_all(self, nb_items_per_page=1000):
         """
         Query all typeform responses.
         Args:
             nb_items_per_page (int): the number of items per page chosen to query typeform responses.
-            last_page (int): last page to query (if None, query all pages).
         Returns:
             List[dict]: the list of responses.
         """
-        data = self.query_page(nb_items_per_page, 1)
-        total_pages = data.json()['page_count']
+        data = self.query_page(nb_items_per_page=nb_items_per_page).json()
+        items = data['items']
+        nb_items = len(items)
 
-        items = data.json()['items']
+        progress_bar = tqdm.tqdm(total=data['page_count'])
 
-        if last_page is None:
-            last_page = total_pages + 1
+        while nb_items > 1:
+            new_items = self.query_page(
+                    nb_items_per_page=nb_items_per_page,
+                    until=items[-1].get('submitted_at')
+                ).json()['items']
+            items.extend(new_items)
 
-        for page_index in tqdm.tqdm(
-                range(2, last_page)
-        ):
-            items.extend(
-                self.query_page(nb_items_per_page, page_index).json()['items']
-            )
+            nb_items = len(new_items)
+            progress_bar.update(1)
+
         return items
