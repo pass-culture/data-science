24a88a0a02f8d8bc64cd4ae48ce7bb5de3effd97
Jean <jeanr@sicara.com>
2020-10-12 15:48:35 +0200

    Review corrections

--- scripts/query_initial_responses.py
+++ scripts/query_initial_responses.py
@@ -7,11 +7,11 @@ import os
 from src.data_access.query_initial_responses import QueryInitialResponses
 
 
-API_KEY = os.getenv('TYPEFORM_API_KEY')
+API_KEY = os.getenv("TYPEFORM_API_KEY")
 
 
 query_responses = QueryInitialResponses("prod", API_KEY)
 responses = query_responses.query_all()
 
-with open('data/api_responses.json', 'w') as json_file:
+with open("data/api_responses.json", "w") as json_file:
     json.dump(responses, json_file)
