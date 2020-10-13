5c215a873b97d96f3a963d5b99cd6b11bd7ed6cc
Jean <jeanr@sicara.com>
2020-10-13 18:27:28 +0200

    (PC-4586): review corrections

--- scripts/query_product_database.py
+++ scripts/query_product_database.py
@@ -1,23 +1,24 @@
-"""
-Query the product database.
-Database schema is available in docs/product_database_schema.png
-"""
 import ast
+import json
 import os
 
 from src.data_access.product_database_connection import ProductDatabaseConnection
+
 
 SSH = os.getenv("PRODUCT_DATABASE_SSH")
 CONNECTION = os.getenv("PRODUCT_DATABASE_CONNECTION")
 
 if type(SSH) == str:
     SSH = ast.literal_eval(SSH)
+else:
+    SSH = json.loads(SSH)
+
 if type(CONNECTION) == str:
     CONNECTION = ast.literal_eval(CONNECTION)
+else:
+    CONNECTION = json.loads(CONNECTION)
 
 product_database_connection = ProductDatabaseConnection(
-    private_key_path="/home/jean/.ssh/id_rsa", **SSH
+    private_key_path="~/id_rsa", **SSH
 )
 dataframe = product_database_connection.query_database(query=f'SELECT * FROM "stock" limit 10;', **CONNECTION)
-
-print(dataframe)
